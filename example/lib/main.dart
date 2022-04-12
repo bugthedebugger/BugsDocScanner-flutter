811f80350fae65c57aaf48f78be2910abfddbd01
BugTheDebugger <bprayush@yahoo.com>
2022-04-12 14:15:34 +0545

    finished basic adapter functions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:bugs_scanner/bugs_scanner.dart';
+import 'package:bugs_scanner/data/scanner_contour.dart';
+import 'package:bugs_scanner/data/scanner_coordinates.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -38,9 +41,28 @@ class _MyAppState extends State<MyApp> {
       http.Response response = await http.get(
         Uri.parse(imageUrl),
       );
-      buffer = await BugsScannerAdapter.getCroppedBWImage(response.bodyBytes);
+      // buffer = await BugsScannerAdapter.cropAndGetColorImageFromBufAsBuf(
+      //   response.bodyBytes,
+      // );
+      buffer = await BugsScannerAdapter
+          .cropAndGetColorImageFromBufAsBufWithCustomContour(
+        buf: response.bodyBytes,
+        contour: ScannerContour.fromEdges(
+          ScannerCoordinates.fromXY(0, 0),
+          ScannerCoordinates.fromXY(0, 150),
+          ScannerCoordinates.fromXY(150, 150),
+          ScannerCoordinates.fromXY(150, 0),
+        ),
+      );
+
+      final c = await BugsScannerAdapter.getContourFromImageBuffer(
+        response.bodyBytes,
+      );
+      print('Contour: ${c.toString()}');
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
+    } catch (e) {
+      platformVersion = e.toString();
     }
 
     // If the widget was removed from the tree while the asynchronous platform
