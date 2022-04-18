a01d94aad07214d5bfce2119d0f1e1b29c6e18bf
BugTheDebugger <bprayush@yahoo.com>
2022-04-18 14:03:29 +0545

    changed extension for file

--- lib/services/bugs_scanner_service.dart
+++ lib/services/bugs_scanner_service.dart
@@ -115,7 +115,7 @@ class BugsScannerService {
       }
 
       File imgFile = File(
-        '${(await getTemporaryDirectory()).path}/${DateTime.now().millisecondsSinceEpoch}.pdf',
+        '${(await getTemporaryDirectory()).path}/${DateTime.now().millisecondsSinceEpoch}.jpg',
       );
 
       imgFile = await imgFile.writeAsBytes(buffer);
