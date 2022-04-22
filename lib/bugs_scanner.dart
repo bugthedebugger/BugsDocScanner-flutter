99b88548b9af96e7f392f126aa8af311d9d9cacb
BugTheDebugger <bprayush@yahoo.com>
2022-04-22 09:10:55 +0545

    adding exports for bindings and scanner service

--- lib/bugs_scanner.dart
+++ lib/bugs_scanner.dart
@@ -1,2 +1,4 @@
 export 'package:bugs_scanner/ffi/bugs_scanner_adapter.dart';
+export 'package:bugs_scanner/ffi/bugs_scanner_bindings.dart';
+export 'package:bugs_scanner/services/bugs_pdf_service.dart';
 export 'package:bugs_scanner/services/bugs_scanner_service.dart';
